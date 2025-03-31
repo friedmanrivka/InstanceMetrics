34990adcd87aa73cf2768713e5f01f5d992558bb
Friedman Rivka <151746698+friedmanrivka@users.noreply.github.com>
2025-03-31 17:53:25 +0300

    Add index file as the application entry point

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,8 @@
-console.log('create server branch');
+import app from './app';
+import dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
